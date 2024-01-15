4c2b3aa041ee1ba4a6ab63d2aeafbc3ed8abd795
SAWANT TUSHAR <sawanttushar796@gmail.com>
2024-01-15 11:44:56 +0530

    first commit

---
+++ src/Components/Blogs.js
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../Context/AppContext'
+import Spinner from './Spinner'
+
+function Blogs() {
+
+  const {loading,posts}=useContext(AppContext)
+
+  return (
+    <div className='w-11/12 max-w-[670px]  py-8 flex flex-col gap-y-7 mt-[66px] mb-[66px] justify-center items-center '>
+{
+  loading ? (<Spinner/>) : (
+    
+      posts.length === 0 ? (
+        <div>
+          <p>No Post Found</p>
+        </div>
+      ) :
+      (
+           
+            posts.map((post)=>(
+              <div key={post.id} >
+                <p className='font-bold text-lg'>{post.title}</p>
+                <p className='text-sm  mt-[4px]'>
+                  By <span className='italic'>{post.author}</span> on <span className='underline font-bold'>{post.category}</span>
+
+                </p>
+
+                <p className='text-sm mt-[4px]'>
+                  Posted on {post.date}
+                </p>
+                <p className='mt-3.5'>{post.content}</p>
+                <div className='flex gap-x-3'>
+                  {
+                    post.tags.map((tag,i)=>(
+                      <span key={i} className='text-blue-700 underline font-bold text-xs mt-[5px] '>{`#${tag}`}</span>
+                    ))
+                  }
+                </div>
+
+              </div>
+
+            ))
+           
+      )
+    
+  )
+}
+    </div>
+  )
+}
+
+export default Blogs
